81158b6a0c9c4b69af04f8e2096f94b53297d152
Leo Dube <15lsd@caslab.queensu.ca>
2020-11-27 15:44:18 -0500

    added dist directory for publishing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {hot} from "react-hot-loader";
+
 import "./App.css";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/projects' component={Projects} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default hot(module)(App);
