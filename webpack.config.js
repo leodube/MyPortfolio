11170e9fc974340e960fd9f55f23fcc231347022
Leo Dube <15lsd@caslab.queensu.ca>
2020-12-01 01:34:37 -0500

    copy _redirects to dist directory on build

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
@@ -70,6 +71,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:"public/index.html",
       favicon: "public/favicon.png",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "public/_redirects", to: "" }
+      ]
     })
   ]
 };
