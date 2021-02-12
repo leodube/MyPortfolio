b0e9ce6260595a9ac6427814f8ac3464da8d85f6
Leo Dube <15lsd@caslab.queensu.ca>
2021-02-12 09:32:24 -0500

    change project subtitles

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,6 +7,7 @@ import websiteImg from "../images/website.png";
 import skedoodleImg from "../images/skedoodle.png";
 
 const Projects = () => {
+
   return (
     <>
       <h1 className="text-left mt-3 mb-5">Projects</h1>
@@ -14,6 +15,7 @@ const Projects = () => {
         <div className="col-lg-6 d-flex">
           <Card
             title="This website!"
+            subtitle="Personal project"
             image={websiteImg}
             description="This responsive single-page application was built using React and Bootstrap."
             btnText="View Code"
@@ -23,6 +25,7 @@ const Projects = () => {
         <div className="col-lg-6 d-flex">
           <Card
             title="Skedoodle"
+            subtitle="Personal project"
             image={skedoodleImg}
             description="Generate random doodle ideas! Built using MongoDB, Express, React and Node.js."
             twoBtns
@@ -35,7 +38,7 @@ const Projects = () => {
         <div className="col-lg-6 d-flex">
           <Card
             title="OTIS Web Application"
-            subtitle="At Resolute FP"
+            subtitle="As Software Developer at Resolute FP"
             image={otisImg}
             description="An intranet-based document management system for pulp and paper mill operators. Built using ASP.NET."
             btnText="Private Code"
@@ -47,7 +50,7 @@ const Projects = () => {
           <Card
             title="StatStick iOS Application"
             image={statstickImg}
-            subtitle="4th Year Capstone Project"
+            subtitle="Computer Engineering Capstone Project"
             description="An iPad application that displays hockey stick action data from an API endpoint. Built using Swift."
             btnText="View Code"
             btnLink="https://github.com/leodube/statstick-app"
@@ -57,7 +60,7 @@ const Projects = () => {
           <Card
             title="StatStick Algorithm"
             image={statstickMLImg}
-            subtitle="4th Year Capstone Project"
+            subtitle="Computer Engineering Capstone Project"
             description="A machine learning algorithm that takes raw sensor data and predicts hockey stick actions using dynamic time warping and fuzzy clustering. Built using Python."
             btnText="View Code"
             btnLink="https://github.com/leodube/statstick-ml"
